6e581d8f4dc7d3eed8d73867f4c3908991159054
Alicia <alicia@icalialabs.com>
2014-12-23 12:46:40 -0600

    Adds ticket_id in response on save method for Report

--- lib/cic/report.rb
+++ lib/cic/report.rb
@@ -3,30 +3,36 @@ require 'cic/active_cic'
 
 module Cic
   class Report < ActiveCic::Base
+    attr_reader :ticket_id
 
     def initialize(hash)
-      super(hash) 
+      super(hash)
     end
 
     def self.find(ticket)
-      response = self.get("/reports.json") 
+      response = self.get("/reports.json")
       if response.success?
-        report_hash = response.parsed_response["reports"].select { |report| report['ticket'] == ticket }.first 
+        report_hash = response.parsed_response["reports"].select { |report| report['ticket'] == ticket }.first
         report_hash ? self.new(report_hash) : nil
        else
         raise_exception(response.code, response.body)
       end
     end
 
-    def save 
-      self.class.post("/reports.json", body: self.raw_attributes ) 
-      true #POST always finds its way...
+    def save
+      response = self.class.post("/reports.json", body: self.raw_attributes )
+      if response.success?
+        @ticket_id = response.parsed_response["reports"]["ticket"]
+        true #POST always finds its way...
+      else
+        false
+      end
     end
 
     def self.where(params = {})
-      response = self.get("/reports.json", query: params) 
+      response = self.get("/reports.json", query: params)
       if response.success?
-        response.parsed_response['reports'].map { |report_hash| self.new(report_hash) } 
+        response.parsed_response['reports'].map { |report_hash| self.new(report_hash) }
        else
         raise_exception(response.code, response.body)
       end
--- spec/report_spec.rb
+++ spec/report_spec.rb
@@ -2,13 +2,23 @@ require 'spec_helper'
 require 'cic/report'
 require 'cic/errors'
 require 'helper'
+require 'active_support/core_ext/object/to_query'
 
 describe Cic::Report do
 
   URL = "http://api.nl.cic.mx/0/nl/reports.json"
 
   before(:each) do
-    @attr =  {"ticket"=>"#7LVB", "content"=>"*ACCIDENTE* En Carretera a Laredo y Jos\u00E9 Clemente Orozco ESC #mtyfollow 11:32 via @escuadronvial", "state"=>"read", "created_at"=>"2013-02-20T11:33:37-06:00", "updated_at"=>"2013-02-20T11:35:20-06:00", "lat"=>"25.79174804389648", "lng"=>"-100.28132557868958", "is_public"=>true, "votes"=>0, "stars"=>0.0, "address_detail"=>{"formatted_address"=>"Jose Clemente Orozco, Sin Nombre de Colonia 17, General Escobedo, NL, M\u00E9xico", "zipcode"=>nil, "county"=>{"long_name"=>"General Escobedo", "short_name"=>"Gral Escobedo"}, "state"=>{"long_name"=>"Nuevo Le\u00F3n", "short_name"=>"NL"}, "neighborhood"=>{"long_name"=>"Sin Nombre de Colonia 17", "short_name"=>"Sin Nombre de Col 17"}}, "group"=>"Vialidad y Transito (SS)", "categories"=>["ACCIDENTE"]}
+    @attr =  { "content" => "*ACCIDENTE* En Carretera a Laredo y Jos\u00E9 Clemente Orozco ESC #mtyfollow 11:32 via @escuadronvial", "state" => "read", "created_at" => "2013-02-20T11:33:37-06:00", "updated_at" => "2013-02-20T11:35:20-06:00", "lat" => "25.79174804389648", "lng" => "-100.28132557868958", "is_public" => true, "votes" => 0, "stars" => 0.0, "address_detail" => {
+      "formatted_address" => "Jose Clemente Orozco, Sin Nombre de Colonia 17, General Escobedo, NL, M\u00E9xico", "zipcode" => nil, "county" => {
+        "long_name" => "General Escobedo", "short_name" => "Gral Escobedo"
+      }, "state" => {
+        "long_name" => "Nuevo Le\u00F3n", "short_name" => "NL"
+      }, "neighborhood" => {
+        "long_name" => "Sin Nombre de Colonia 17", "short_name" => "Sin Nombre de Col 17"
+      }
+    }, "group" => "Vialidad y Transito (SS)", "categories" => ["ACCIDENTE"]
+  }
   end
 
   subject { Cic::Report.new(@attr) }
@@ -19,14 +29,17 @@ describe Cic::Report do
 
   describe '#find' do
 
+    before(:each) do
+      @find_attr = @attr.merge({ "ticket" => "#A2S3" })
+    end
     context 'when response is success' do
       it 'returns the record specified by the ticket id' do
-        mock_request(URL, { reports: [@attr] })
-        Cic::Report.find(@attr['ticket']).ticket.should eql @attr['ticket']
+        mock_request(URL, { reports: [@find_attr] })
+        Cic::Report.find(@find_attr['ticket']).ticket.should eql @find_attr['ticket']
       end
 
       it 'returns nil when no record found' do
-        mock_request(URL, { reports: [@attr] })
+        mock_request(URL, { reports: [@find_attr] })
         Cic::Report.find('#1234').should be_nil
       end
     end
@@ -101,19 +114,23 @@ describe Cic::Report do
   describe '#save' do
     before(:each) do
       @params = {
-        content: 'Prueba Kuri jalo',
-        address: 'cataluna 206',
-        origin: 'mailto:jsmith@example.com',
-        return_path: 'mailto:jsmith@example.com',
-        lat: '25.67316978132684',
-        lng: '-100.35339117050171',
-        address: 'cataluna 206',
-        assets: 'http://icalialabs.com/img/logo.png'
+        content: 'Lorem'
       }
+      @report = Cic::Report.new(@attr)
+      stub_request(:post, "http://api.nl.cic.mx/0/nl/reports.json").
+         with(:body => "content=*ACCIDENTE*%20En%20Carretera%20a%20Laredo%20y%20Jos%C3%A9%20Clemente%20Orozco%20ESC%20%23mtyfollow%2011%3A32%20via%20%40escuadronvial&state=read&created_at=2013-02-20T11%3A33%3A37-06%3A00&updated_at=2013-02-20T11%3A35%3A20-06%3A00&lat=25.79174804389648&lng=-100.28132557868958&is_public=true&votes=0&stars=0.0&address_detail[formatted_address]=Jose%20Clemente%20Orozco%2C%20Sin%20Nombre%20de%20Colonia%2017%2C%20General%20Escobedo%2C%20NL%2C%20M%C3%A9xico&address_detail[zipcode]=&address_detail[county][long_name]=General%20Escobedo&address_detail[county][short_name]=Gral%20Escobedo&address_detail[state][long_name]=Nuevo%20Le%C3%B3n&address_detail[state][short_name]=NL&address_detail[neighborhood][long_name]=Sin%20Nombre%20de%20Colonia%2017&address_detail[neighborhood][short_name]=Sin%20Nombre%20de%20Col%2017&group=Vialidad%20y%20Transito%20(SS)&categories[]=ACCIDENTE",
+              :headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+         to_return(:status => 200, :body => {reports: { ticket: "#123" }}.to_json, :headers => {"Content-Type" => 'application/json'})
     end
+
     it 'returns true if report was saved successfully' do
-      report = Cic::Report.new(@params)
-      report.save.should be_true
+      expect(report.save).to eql(true)
     end
+
+    it 'returns a ticket number on the response' do
+      expect{ @report.save }.to change{ @report.ticket_id }.from(nil).to("#123")
+    end
+
+
   end
 end
